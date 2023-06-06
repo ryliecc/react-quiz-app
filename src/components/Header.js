c76607b4bbfb2fbc8488bd487b8d39d5c481b8c5
Rylie Castell <rylie.castell@gmail.com>
2023-06-06 09:31:40 +0200

    initial setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header>
-      <h1>Rylie's QuizApp</h1>
+    <header className="header">
+      <h1 className="header__title">Rylie's QuizApp</h1>
     </header>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,15 @@
 export default function NavBar() {
-  return <nav></nav>;
+  return (
+    <nav className="nav-bar">
+      <button type="button" className="nav-bar__button">
+        🏠
+      </button>
+      <button type="button" className="nav-bar__button">
+        🤍
+      </button>
+      <button type="button" className="nav-bar__button">
+        👤
+      </button>
+    </nav>
+  );
 }
