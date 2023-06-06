069c722bedadd5afd502a1a553ac2307431af605
Rylie Castell <rylie.castell@gmail.com>
2023-06-06 13:24:20 +0200

    add navigation feature

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,13 @@
-export default function NavBar() {
+export default function NavBar({ onGoHome, onGoBookmarks, onGoCardForm }) {
   return (
     <nav className="nav-bar">
-      <button type="button" className="nav-bar__button">
+      <button type="button" className="nav-bar__button" onClick={onGoHome}>
         🏠
       </button>
-      <button type="button" className="nav-bar__button">
+      <button type="button" className="nav-bar__button" onClick={onGoBookmarks}>
         🤍
       </button>
-      <button type="button" className="nav-bar__button">
+      <button type="button" className="nav-bar__button" onClick={onGoCardForm}>
         ➕
       </button>
     </nav>
