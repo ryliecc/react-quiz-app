bb647b9ce5899cc5822a6a9a2be70daafc0c9752
Rylie Castell <rylie.castell@gmail.com>
2023-06-10 00:59:22 +0200

    style navigation bar

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,6 +2,10 @@ export default function Settings() {
   return (
     <section className="settings">
       <h2>Settings</h2>
+      <ul>
+        <li>Dark Mode</li>
+        <li>Color Theme</li>
+      </ul>
     </section>
   );
 }
