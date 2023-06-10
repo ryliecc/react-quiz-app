83c5495a23bfe640abde62825dac68737350c7b5
Rylie Castell <rylie.castell@gmail.com>
2023-06-10 05:15:50 +0200

    add content to settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,11 +1,29 @@
+import { useState } from "react";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
+
 export default function Settings() {
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  function toggleDarkMode() {
+    setDarkMode(!isDarkMode);
+  }
   return (
     <section className="settings">
       <h2>Settings</h2>
-      <ul>
-        <li>Dark Mode</li>
-        <li>Color Theme</li>
-      </ul>
+      <p>Dark Mode</p>
+      <DarkModeSwitch
+        style={{ marginBottom: "2rem" }}
+        checked={isDarkMode}
+        onChange={toggleDarkMode}
+        size={50}
+        sunColor="white"
+      />
+      <p>
+        Thank you for checking my App out! I created it to practice React during
+        my Web Development Bootcamp. If you like it, you might want to take a
+        look at my <a href="https://github.com/ryliecc">GitHub Profile</a> as
+        well.
+      </p>
     </section>
   );
 }
