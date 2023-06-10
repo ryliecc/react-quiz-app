6db259a818da5c5197960c9a954503539850ff32
Rylie Castell <rylie.castell@gmail.com>
2023-06-10 07:12:58 +0200

    fix settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,29 +1,11 @@
-import { useState } from "react";
-import { DarkModeSwitch } from "react-toggle-dark-mode";
-
 export default function Settings({ showSettings }) {
-  const [isDarkMode, setDarkMode] = useState(false);
-
-  function toggleDarkMode() {
-    setDarkMode(!isDarkMode);
-  }
   return (
     <section
       className="settings"
       style={{ visibility: showSettings ? "visible" : "collapse" }}
     >
       <h2 className="settings__heading">Settings</h2>
-      <p className="settings__dark-mode">
-        Dark Mode:
-        <DarkModeSwitch
-          style={{ marginBottom: "2rem" }}
-          checked={isDarkMode}
-          onChange={toggleDarkMode}
-          size={50}
-          sunColor="#abb1cf"
-          className="settings__dark-mode-switch"
-        />
-      </p>
+      <p className="settings__dark-mode">Dark Mode:</p>
       <p className="settings__epilogue">
         Thank you for checking my App out!
         <br />I created it to practice React during my Web Development Bootcamp.
