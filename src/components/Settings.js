ca2a03de5981c32c46bb26d5d92e413260420fdc
Rylie Castell <rylie.castell@gmail.com>
2023-06-14 03:27:08 +0200

    add dark mode feature

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,11 +1,11 @@
-export default function Settings({ showSettings }) {
+export default function Settings({ showSettings, children }) {
   return (
     <section
       className="settings"
       style={{ visibility: showSettings ? "visible" : "collapse" }}
     >
       <h2 className="settings__heading">Settings</h2>
-      <p className="settings__dark-mode">Dark Mode:</p>
+      <p className="settings__dark-mode">Dark Mode: {children}</p>
       <p className="settings__epilogue">
         Thank you for checking my App out!
         <br />I created it to practice React during my Web Development Bootcamp.
